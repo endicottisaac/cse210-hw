50274c77a42535c7ef9251f46129c363ad3130f4
endicottisaac <endicottkai@gmail.com>
2026-03-07 13:27:57 -0700

    completed exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,38 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+    {   
+        Console.WriteLine("Welcome to the Number Guessing Game!");
+        Console.Write("Would you like to play? (yes/no) ");
+        string playGame = Console.ReadLine();
+        while (playGame == "yes")
+        { 
+            Random random = new Random();
+            int randomNumber = random.Next(1, 101);
+            int userGuess = 0;
+            int numberOfGuesses = 0;
+            Console.WriteLine("Please guess a number between 1 and 100.");
+            while (userGuess != randomNumber)
+            {
+                Console.Write("Enter your guess: ");
+                userGuess = int.Parse(Console.ReadLine());
+                numberOfGuesses++;
+
+                if (userGuess < randomNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (userGuess > randomNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {numberOfGuesses} guesses!");
+                }
+            }
+            Console.Write("Would you like to play again? (yes/no) ");
+            playGame = Console.ReadLine(); 
+        }
     }
 }
