4021ee14477b6be3e6d19a1438b49762a516931c
endicottisaac <endicottkai@gmail.com>
2026-04-09 14:37:49 -0600

    w05 learning activities

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,25 @@
+namespace Homework
+{
+    public class Assignment
+    {
+        private string _studentName;
+        private string _topic;
+
+        public Assignment(string studentName, string topic)
+        {
+            _studentName = studentName;
+            _topic = topic;
+        }
+
+        public string GetSummary()
+        {
+            return $"{_studentName} - {_topic}";
+        }
+
+        // Getter for student name for derived classes
+        public string GetStudentName()
+        {
+            return _studentName;
+        }
+    }
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // assignment base class
+        Homework.Assignment assignment = new Homework.Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        // math assignment derived class
+        Homework.MathAssignment mathAssignment = new Homework.MathAssignment(
+            "Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        // writing assignment derived class
+        Homework.WritingAssignment writingAssignment = new Homework.WritingAssignment(
+            "Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
